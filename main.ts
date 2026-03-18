59a0c4565c4256fa87dde863b031b96ed2b58e98
Stefania Formato <sformato95@gmail.com>
2026-03-18 23:24:07 +0100

    Add function for generate random number

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+
+function generateRandomNumber(min: number, max: number) {
+    const randomNubmerGenrator = Math.random();
+
+
+
+    return Math.trunc(randomNubmerGenrator * (max - min) + min);
+   ;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log("Random number:", generateRandomNumber(1, 5));
+}
