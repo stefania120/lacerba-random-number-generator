ea920be8489a957c1fc8e3d0a9bbd20b777b1e29
Stefania Formato <sformato95@gmail.com>
2026-03-19 00:15:26 +0100

    Add lists for generate random number in specific range of number

--- main.ts
+++ main.ts
@@ -1,22 +1,20 @@
-
 function randomNubmerGenrator(min: number, max: number) {
-    const randomNubmerGenrator = Math.random();
+  const randomNubmerGenrator = Math.random();
 
-    // randomNubmerGenrator * (max - min) + min
-    // randomNubmerGenrator = 0 => min
-    // randomNubmerGenrator = 1 => max
+  // randomNubmerGenrator * (max - min) + min
+  // randomNubmerGenrator = 0 => min
+  // randomNubmerGenrator = 1 => max
 
-    return Math.trunc(randomNubmerGenrator * (max - min) + min);
-   ;
+  return Math.trunc(randomNubmerGenrator * (max - min) + min);
 }
 
-
-
-function RNGDec(min: number, max: number, precision: number) {
-    const multFactor = Math.pow(10, precision);
-    return randomNubmerGenrator(min * multFactor, max * multFactor) / multFactor;
+function RNGSequence(len: number, min: number, max: number) {
+  const res: number[] = [];
+  for (let i = 0; i < len; i++) {
+    res.push(randomNubmerGenrator(min, max));
+  }
+  return res;
 }
 
-console.log("Random number with decimals:", RNGDec(0, 10, 1)); // 2.4 
-console.log("Random number with decimals:", RNGDec(0, 10, 2)); // 2.45
-console.log("Random number with decimals:", RNGDec(5, 10, 5)); // 2.45678
+const myArr = RNGSequence(100, 5, 10);
+console.log(myArr);
